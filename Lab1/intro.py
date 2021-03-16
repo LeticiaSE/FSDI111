4911a0395b2a6e6b9aaa62055ed99ae37ebb4b60
LeticiaSE <35822181+LeticiaSE@users.noreply.github.com>
2021-03-16 02:44:34 -0700

    Assignment 2

---
+++ Lab1/intro.py
@@ -0,0 +1,39 @@
+# functions
+def print_separador():
+    print('--------')
+    print('here')
+
+# instruction
+print('Hello World')
+
+# variables
+name = 'Letty'
+age = 27
+total = 77.89
+found = True
+
+print(name)
+print(age)
+print(total)
+
+print_separador()
+
+#math operations
+print(age - 10)
+print(age + 10)
+print(2 * 4)
+print(10 / 5)
+print(33 % 2) #mod operator
+
+print_separador()
+
+# if statements
+
+user_age = 52:
+if (user_age < 80):
+    print('you are still young')
+    print('Don´t worry too much')
+elif user_age == 80:
+    print('you are still young')
+    print('Don´t worry too much')
+else
